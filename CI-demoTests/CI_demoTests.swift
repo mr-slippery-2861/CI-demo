3882498f33a0d44602ee9cff2e532c5d61659a17
Lex Chen <lexchen@mooyoo.com.cn>
2016-08-02 23:30:42 +0800

    test ut

--- CI-demoTests/CI_demoTests.swift
+++ CI-demoTests/CI_demoTests.swift
@@ -10,20 +10,24 @@ import XCTest
 @testable import CI_demo
 
 class CI_demoTests: XCTestCase {
-    
+    var vc: ViewController?
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        vc = ViewController()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        vc = nil
     }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let c = vc!.test(a: 1, b: 2)
+        XCTAssertEqual(3, c)
     }
     
     func testPerformanceExample() {
